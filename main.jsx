c9526e4c72eefe1c1cc5d00227cb3a7412a6dfdd
annadasinfo-cyber <annadas.info@etik.com>
2026-07-15 15:32:27 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import StricklieselApp from "./stricklisel-app.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<StricklieselApp />);
